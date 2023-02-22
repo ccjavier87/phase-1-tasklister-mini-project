beba783e07f8f15325b2ff6305a587f762570630
Carl_Javier <ccjavier87@gmail.com>
2023-02-21 16:01:32 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+   let form = document.querySelector('form')
+   form.addEventListener('submit', (e) => {
+     e.preventDefault();
+     makeDo(e.target.newTaskDescription.value)
+     form.reset()
+    
+  }) 
+
+  function makeDo(toDo){
+  
+  let p = document.createElement('p')
+  let x = document.createElement('button')
+  x.addEventListener('click', handleDelete)
+  x.textContent = 'x'
+  p.textContent = `${toDo}`
+  p.appendChild(x)
+  document.querySelector('#tasks').appendChild(p)
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+
+
 });
